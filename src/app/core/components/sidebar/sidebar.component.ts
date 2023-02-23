e69293818158b91c0ec8e55ec65ad0d59912d82a
LuizHenriqueT1 <luizhenrique201387@gmail.com>
2023-02-23 16:16:37 -0300

    menu-burgue para mobile

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map, Observable, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private breakpointObservable: BreakpointObserver,
+  ) { }
+  isHandset$: Observable<boolean> = this.breakpointObservable.observe(Breakpoints.XSmall)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay());
+
+  isHandsetW$: Observable<boolean> = this.breakpointObservable.observe([Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large, Breakpoints.XLarge])
+    .pipe(
+      map((result) => result.matches),
+      shareReplay());
+
+  buttonClick: boolean = false;
+  buttonMenu() {
+    this.buttonClick = !this.buttonClick;
+  }
+
 
   ngOnInit(): void {
   }
