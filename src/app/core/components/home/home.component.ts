d875755a65b2ef836f72a660f257823af57baaae
LuizHenriqueT1 <luizhenrique201387@gmail.com>
2023-02-23 20:54:41 -0300

    Responsividade completa

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map, Observable, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private breakpointObservable: BreakpointObserver,
+  ) { }
+  windowScrolled: boolean = false;
+
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    if (window.pageYOffset > 100) {
+      this.windowScrolled = true;
+    } else {
+      this.windowScrolled = false;
+    }
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  isHandset$: Observable<boolean> = this.breakpointObservable.observe(Breakpoints.XSmall)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay());
+
+  isHandsetW$: Observable<boolean> = this.breakpointObservable.observe([Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large, Breakpoints.XLarge])
+    .pipe(
+      map((result) => result.matches),
+      shareReplay());
+
 
   ngOnInit(): void {
   }
